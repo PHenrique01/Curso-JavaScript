31e4bb5b63db2b3c699bc2adb122930122b4ba92
PHenrique01 <paulinho.pawer14@gmail.com>
2023-02-22 20:19:02 -0300

    Atualização de javascript - 05

---
+++ Desafio/desafios/js/script01.js
@@ -0,0 +1,11 @@
+function botao1(){
+    window.alert('Você clicou no 1ª botão!')
+}
+
+function botao2(){
+    window.alert('Você clicou no 2ª botão!')
+}
+
+function botao3(){
+    window.alert('Você clicou no 3ª botão!')
+}
---
+++ Desafio/desafios/js/script06.js
@@ -0,0 +1,14 @@
+function converter(){
+    let celsius = window.prompt('Digite uma temperatura em celsius')
+    let elemento = document.createElement('p')
+    elemento.innerHTML = `A temperatura de ${celsius} celsius, corresponde a...`
+    let res = document.getElementById('res')
+    
+    let kelvin = Number(celsius) + 273.15
+    let faren = Number(celsius)*9/5 + 32
+   
+    elemento.innerHTML += `<br>${kelvin} kelvin(s)`
+    elemento.innerHTML += `<br>${faren} farenheit(s)`
+    
+    res.appendChild(elemento)
+}
