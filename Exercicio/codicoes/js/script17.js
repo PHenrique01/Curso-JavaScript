3b1a2c16b5dbc86f149395ce4b8e706e37a05270
PHenrique01 <paulinho.pawer14@gmail.com>
2023-03-30 10:48:00 -0300

    Atualização de javascript - 08

--- Exercicio/codicoes/js/script17.js
+++ Exercicio/codicoes/js/script17.js
@@ -1,7 +1,19 @@
 function gerar(){
-    window.alert('[ERRO]!! ainda não conclui esse exercicio.')
+    // window.alert('[ERRO]!! ainda não conclui esse exercicio.')
+
+    let min = 1
+    let max = 100
+    let dif = max - min
+    let aleatorio = Math.random()
+    let num = min + Math.trunc(dif * aleatorio)
+
+    let res = document.querySelector('div#res')
+    res.innerHTML += `<p>Acabei de pensar no número <mark>${num}</mark>!</p>`
 }
 
 function limpar(){ 
-    window.alert('[ERRo]!! ainda não conclui esse exercicio.')
+    // window.alert('[ERRo]!! ainda não conclui esse exercicio.')
+
+    let res = document.querySelector('div#res')
+    res.innerHTML = null
 }
--- Exercicio/codicoes/js/script18.js
+++ Exercicio/codicoes/js/script18.js
@@ -1,3 +1,26 @@
-function sorteio(){
-    window.alert('[ERRO]!! ainda não conclui esse exercicio.')
+let computador = 0
+let jogador = 0
+let res = document.getElementById('res')
+
+function sortear(){
+    // window.alert('[ERRO]!! ainda não conclui esse exercicio.')
+
+    let min = 1
+    let max = 100
+    let dif = max - min
+    let aleatorio = Math.random()
+    computador = min + Math.trunc(dif * aleatorio) 
+}
+
+function jogar(){
+    jogador = Number(window.prompt('Qual é o seu palpite?'))
+
+    if(jogador < computador){
+        res.innerHTML += `<p>Você falou ${jogador}. Meu número é <strong>Maior</strong>!</p>`
+    }else if(jogador > computador){
+        res.innerHTML += `<p>Você falou ${jogador}. Meu número é <strong>Menor</strong>!</p>`
+    }else if(jogador == computador){
+        res.innerHTML += `<p><strong>PARABÉNS!</strong> Você acertou! Eu tinha sorteado o valor ${computador}!</p>`
+        document.getElementById('btn').style.visibility = 'hidden'
+    }
 }
